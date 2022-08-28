06b2cb8a483f7e40c7d50646a8ac7ab52b4f2e9a
Zhenya <sverhzad@gmail.com>
2022-08-28 12:09:09 +0300

    edit Procfile

--- config.py
+++ config.py
@@ -10,5 +10,9 @@ class BaseConfig:
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or \
-        f'postgresql://{os.environ.get("LOGIN")}:{os.environ.get("PASSWORD")}@localhost/jokes_peeper'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://zhenyavo:dun5k466@localhost/jokes_peeper'
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('LOGIN')}:{os.environ.get('PASSWORD')}@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/dd8208nm4umc0i"
