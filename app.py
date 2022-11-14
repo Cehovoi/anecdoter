0b834c5856ef2a98c33b5bd7cb5dcacfee408168
zhenya <sverhzad@gmail.com>
2022-11-14 12:55:08 +0300

    working aiogram on server with web hook

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ def cli():
 def aiobot(token, hook_host=None):
     if not hook_host:
         from anecdoter.consts import DOMAIN
-        hook_host = DOMAIN
+        hook_host = f'https://{DOMAIN}'
     from anecdoter.bot_app import AioBot
     aio_bot = AioBot(token=token,
                      web_hook_host=hook_host,
                      web_hook_path='/',
                      web_app_host='0.0.0.0',
-                     web_app_port=8444,
+                     web_app_port=8443,
                      )
     aio_bot.run_aiogram()
 
@@ -28,14 +28,13 @@ def aiobot(token, hook_host=None):
 @click.option('--token_2', envvar="TOKEN_2")
 def telebot(token_2):
     from anecdoter.bot_app import TeleBot
-    from requests.exceptions import ConnectionError
     from anecdoter.cache import cache
     tele_bot = TeleBot(token=token_2)
     try:
         tele_bot.run_telebot()
-    except ConnectionError:
+    except Exception as e:
         status = cache.drop_all_cache_to_db()
-        print("ConnectionError ocure\n"*10, status)
+        print("TELEBOT fell\n"*10, e, 'drop to db',  status)
         tele_bot.run_telebot()
 
 
